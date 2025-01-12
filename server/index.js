e6a146530b9fac4433edaea76b12872f1f7401c1
Deepanshu <153765399+Deepanshu00-1@users.noreply.github.com>
2025-01-13 02:03:51 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: ["https://mern-crud-frontend-kappa.vercel.app"],
+        origin: ["https://mern-crud-frontend2.vercel.app"],
         methods: ["POST", "GET", "PUT", "DELETE"],
         credentials: true
     }
